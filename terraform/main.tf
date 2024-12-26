bd210576018006b1ef87bb88b57c5c65015cb2e4
samkons <konzisamuel@gmail.com>
2024-12-26 19:30:16 +0100

    move backemd to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,19 @@
-# terraform {
-#   backend "s3" {
-#     bucket = var.infra_bucket
-#     key = "reddit-backend/terraform.tfstate"
-#     region = "eu-central-1"
-#     dynamodb_table = "terraform-lock"
-#   }
-#
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "car-predictor"
+    key = "reddit-backend/terraform.tfstate"
+    region = "eu-central-1"
+  }
 
-provider "aws" {
-  region  = var.region
-  # profile = "default"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+
 
 
 
