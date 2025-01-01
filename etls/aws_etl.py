73222b2d0dd0d4fe8152b9050c047eae364cd139
samkons <konzisamuel@gmail.com>
2025-01-01 21:46:39 +0100

    add changes to envs and airflow deplyoment

--- etls/aws_etl.py
+++ etls/aws_etl.py
@@ -1,5 +1,5 @@
 import s3fs
-from utils.constants import AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY
+from utils.constants import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 
 print("ke is->", AWS_ACCESS_KEY_ID)
@@ -7,7 +7,7 @@ def connect_to_s3():
     try:
         s3 = s3fs.S3FileSystem(anon=False,
                                key= AWS_ACCESS_KEY_ID,
-                               secret=AWS_ACCESS_KEY)
+                               secret=AWS_SECRET_ACCESS_KEY)
         return s3
     except Exception as e:
         print(e)
--- utils/constants.py
+++ utils/constants.py
@@ -7,28 +7,38 @@ load_dotenv()
 parser = configparser.ConfigParser()
 # parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
 
-parser.set('api_keys', 'reddit_secret_key', os.getenv('REDDIT_SECRET_KEY', parser.get('api_keys', 'reddit_secret_key')))
-parser.set('api_keys', 'reddit_client_id', os.getenv('REDDIT_CLIENT_ID', parser.get('api_keys', 'reddit_client_id')))
+# parser.set('api_keys', 'reddit_secret_key', os.getenv('REDDIT_SECRET_KEY'))
+# parser.set('api_keys', 'reddit_client_id', os.getenv('REDDIT_CLIENT_ID'))
+#
+# parser.set('aws', 'aws_access_key_id', os.getenv('AWS_ACCESS_KEY_ID'))
+# parser.set('aws', 'aws_secret_access_key', os.getenv('AWS_SECRET_ACCESS_KEY'))
+# parser.set('aws', 'aws_region', os.getenv('AWS_REGION'))
+# parser.set('aws', 'aws_bucket_name', os.getenv('AWS_BUCKET_NAME'))
+#
+#
+# # Now you can use the updated configuration as normal
+# SECRET = parser.get('api_keys', 'reddit_secret_key')
+# CLIENT_ID = parser.get('api_keys', 'reddit_client_id')
+#
+#
+# # AWS Configuration
+# AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
+# AWS_SECRET_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
+# AWS_REGION = parser.get('aws', 'aws_region')
+# AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
+#
+# INPUT_PATH = parser.get('file_paths', 'input_path', fallback='/opt/airflow/data/input')
+# OUTPUT_PATH = parser.get('file_paths', 'output_path', fallback='/opt/airflow/data/output')
 
-parser.set('aws', 'aws_access_key_id', os.getenv('AWS_ACCESS_KEY_ID', parser.get('aws', 'aws_access_key_id')))
-parser.set('aws', 'aws_secret_access_key', os.getenv('AWS_SECRET_ACCESS_KEY', parser.get('aws', 'aws_secret_access_key')))
-parser.set('aws', 'aws_region', os.getenv('AWS_REGION', parser.get('aws', 'aws_region')))
-parser.set('aws', 'aws_bucket_name', os.getenv('AWS_BUCKET_NAME', parser.get('aws', 'aws_bucket_name')))
+SECRET = os.getenv('REDDIT_SECRET_KEY', 'default_secret_key')
+CLIENT_ID = os.getenv('REDDIT_CLIENT_ID', 'default_client_id')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'default_access_key')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', 'default_secret_key')
+AWS_REGION = os.getenv('AWS_REGION', 'default_region')
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME', 'default_bucket_name')
 
-
-# Now you can use the updated configuration as normal
-SECRET = parser.get('api_keys', 'reddit_secret_key')
-CLIENT_ID = parser.get('api_keys', 'reddit_client_id')
-
-
-# AWS Configuration
-AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
-AWS_SECRET_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
-AWS_REGION = parser.get('aws', 'aws_region')
-AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
-
-INPUT_PATH = parser.get('file_paths', 'input_path', fallback='/opt/airflow/data/input')
-OUTPUT_PATH = parser.get('file_paths', 'output_path', fallback='/opt/airflow/data/output')
+INPUT_PATH = os.getenv('INPUT_PATH', '/opt/airflow/data/input')
+OUTPUT_PATH = os.getenv('OUTPUT_PATH', '/opt/airflow/data/output')
 
 POST_FIELDS = (
     'id',
@@ -43,3 +53,4 @@ POST_FIELDS = (
     'spoiler',
     'stickied'
 )
+
