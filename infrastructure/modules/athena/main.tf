fec1aefe336902812a89811cc907d9ba6913d2d9
samkons <konzisamuel@gmail.com>
2025-01-09 23:53:00 +0100

    athena main .tf changes

--- infrastructure/modules/athena/main.tf
+++ infrastructure/modules/athena/main.tf
@@ -3,10 +3,3 @@ resource "aws_athena_database" "reddit_athena_db" {
   # bucket = "s3://${var.s3_bucket}/athena_scripts/"
 }
 
-# output "athena_query_example" {
-#   value = <<EOT
-#   SELECT *
-#   FROM "${aws_glue_catalog_database.reddit_db.name}"."transformed_latest"
-#   LIMIT 10;
-# EOT
-# }
